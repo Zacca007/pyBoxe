717fc09cb3b552c600b5da8e56c45bdcfcc60b87
Pietro Zaccarini <pietrozaccarini6@gmail.com>
2025-02-23 11:36:52 +0100

    added icon and

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,4 +1,5 @@
 from .netManager import NetManager
 from .window import MyWindow
+from .dataManager import DataManager
 
-__all__ = ["NetManager", "MyWindow"]
+__all__ = ["NetManager", "MyWindow", "DataManager"]
